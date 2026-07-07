9e4864a1f926de5ba6eacc03ad6c651f2e796dd9
Tihomir Marinov <Tihomir.Marinov21@outlook.com>
2026-07-07 14:19:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ function addMessage(text) {
         now.getMinutes().toString().padStart(2, "0");
 
     div.className = "message";
-    div.innerText = text + " ["                                     + time + "]";
+    div.innerText = text + " [" + time + "]";
 
     messages.appendChild(div);
 
