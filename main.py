7840b45c54dca02c08329601a142cd8d5f69f0ae
salvatore813 <luho2504@gmail.com>
2026-07-12 11:37:15 +0200

    added comments

--- main.py
+++ main.py
@@ -5,36 +5,36 @@ import os
 
 PORT = int(os.environ.get("PORT", 8765))
 
-clients = set()
+clients = set() # erstelle eine Menge, um die verbundenen Clients zu speichern
 
-async def handler(websocket):
+async def handler(websocket):   # definiere asynchrone handler funktion
 
-    clients.add(websocket)
+    clients.add(websocket)      # neue websocket verbindung wird als client zum Set hinzugefügt
 
     try:
-        async for message in websocket:
+        async for message in websocket:     # async endlosschleife während client verbunden ist um nachrichten des clients zu lesen
 
-            print(f"Received: {message}")
+            print(f"Received: {message}")   # console log
 
-            for client in clients:
-                await client.send(message)
+            for client in clients:          # wenn eine nachricht kommt, wird sie an jeden verbundenen client gesendet (einschließlich Sender)
+                await client.send(message)  
 
     except:
         pass
 
-    finally:
+    finally:                                # wenn fehler, entferne client aus dem Set
         clients.remove(websocket)
 
-async def main():
+async def main():                           # definiere asynchrone main function
 
-    server = await websockets.serve(
+    server = await websockets.serve(        # server erstellen mit handler, host und PORT
         handler,
         "0.0.0.0",
         PORT
     )
 
-    print(f"Server started on port {PORT}")
+    print(f"Server started on port {PORT}") # console log
 
-    await server.wait_closed()
+    await server.wait_closed()              # warte auf beenden aller verbindungen, dann schließe den server
 
 asyncio.run(main())
